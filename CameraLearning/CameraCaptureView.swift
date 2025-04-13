d2a476a3364b1cdc10f2f7507f8eb9e63b7f11cb
Saikaku <grnmnsta@gmail.com>
2025-04-13 19:05:57 +0900

    カメラ実装

---
+++ CameraLearning/CameraCaptureView.swift
@@ -0,0 +1,47 @@
+//
+//  CameraCaptureView.swift
+//  CameraLearning
+//
+//  Created by cmStudent on 2025/04/13.
+//
+
+import SwiftUI
+
+struct CameraCaptureView: UIViewControllerRepresentable {
+    @Binding var image: UIImage? //储存拍摄的图片
+    @Environment(\.presentationMode) private var presentationMode //控制View的展示和关闭
+
+    func makeUIViewController(context: Context) -> UIImagePickerController {
+        let picker = UIImagePickerController()
+        picker.sourceType = .camera
+        picker.delegate = context.coordinator
+        picker.allowsEditing = true
+        return picker
+    }
+
+    func updateUIViewController(_ uiViewController: UIImagePickerController, context: Context) { }
+
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+
+    class Coordinator: NSObject, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+        let parent: CameraCaptureView
+
+        init(_ parent: CameraCaptureView) {
+            self.parent = parent
+        }
+
+        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+            if let uiImage = info[.originalImage] as? UIImage {
+                parent.image = uiImage
+            }
+
+            parent.presentationMode.wrappedValue.dismiss()
+        }
+
+        func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+            parent.presentationMode.wrappedValue.dismiss()
+        }
+    }
+}
--- CameraLearning/ContentView.swift
+++ CameraLearning/ContentView.swift
@@ -8,14 +8,48 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var image: UIImage?
+    @State private var showImagePickerDialog = false
+    @State private var showCamera: Bool = false
+    @State private var showLibrary: Bool = false
+    @State private var icon:UIImage?
+    
     var body: some View {
+        
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!123")
+            Button(action: {
+                showImagePickerDialog = true
+            }) {
+                Text("編集する")
+                    .underline()
+            }
         }
-        .padding()
+        .sheet(isPresented: $showLibrary) {
+            PhotoLibraryPickerView(image: Binding(
+                get: { icon },
+                set: { icon = $0 }
+            ))
+        }
+        .fullScreenCover(isPresented: $showCamera) {
+            CameraCaptureView(image: $image)
+                .ignoresSafeArea()
+        }
+        .confirmationDialog("",isPresented: $showImagePickerDialog,titleVisibility: .hidden) {
+            Button {
+                showCamera = true
+            } label: {
+                Text("カメラで撮る")
+            }
+            Button {
+                showLibrary = true
+            } label: {
+                Text("アルバムから選ぶ")
+            }
+            Button("キャンセル", role: .cancel) {
+                showImagePickerDialog = false
+            }
+        }
     }
 }
 
