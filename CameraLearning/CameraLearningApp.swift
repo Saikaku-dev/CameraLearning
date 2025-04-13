0846394cc8d9c4f2a29451bc450994729b022a1c
Saikaku <grnmnsta@gmail.com>
2025-04-13 17:38:27 +0900

    Initial Repo

---
+++ CameraLearning/CameraLearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  CameraLearningApp.swift
+//  CameraLearning
+//
+//  Created by cmStudent on 2025/04/13.
+//
+
+import SwiftUI
+
+@main
+struct CameraLearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
